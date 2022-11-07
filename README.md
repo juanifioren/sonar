69401c50a2b7fcdbeaa4cd803e8e1d370d11778d
Juan Ignacio Fiorentino <juanifioren@gmail.com>
2022-11-07 00:21:16 -0300

    Edit fake data command.

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ Useful commands to install and run the project. First install Docker.
 ```
 $ docker-compose -f docker-compose.yaml build
 $ docker-compose -f docker-compose.yaml up
-$ docker-compose -f docker-compose.yaml migrate
+$ docker-compose -f docker-compose.yaml run --rm web python manage.py migrate
+$ docker-compose -f docker-compose.yaml run --rm web python manage.py createfakedata
 ```
 
 Run tests.
@@ -28,3 +29,7 @@ Here I am going to leave a list of things that I was thinking to improve in a fu
 - Use translate tag on templates and generate translation files (also ensure is used on views).
 - Maybe cache views count.
 - Improve post view count logic, maybe using some cache system to stop hitting db to check if was viewed already.
+- Improve dashboard data with caching system.
+- Generate better data for Activity Logs when using manage command.
+
+_Created by Juan Ignacio Fiorentino._
--- sonar/management/commands/createfakedata.py
+++ sonar/management/commands/createfakedata.py
@@ -31,7 +31,7 @@ class Command(BaseCommand):
         if not User.objects.exclude(is_superuser=True).exists():
             self.stdout.write('Creating random users. This could take a few seconds...')
             users = []
-            for _ in range(100):
+            for _ in range(99):
                 profile = fake.simple_profile()
                 user = User()
                 user.username = profile['username']
@@ -48,6 +48,8 @@ class Command(BaseCommand):
 
         # Create random posts.
         if not Post.objects.exists():
+            self.stdout.write('Creating random posts. This could take a few seconds...')
+
             post_images = cycle([
                 'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',  # noqa
                 'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',  # noqa
@@ -61,3 +63,29 @@ class Command(BaseCommand):
 
         else:
             self.stdout.write(self.style.WARNING('Posts already created.'))
+
+        # Create random activity.
+        if not ActivityLog.objects.exists():
+            random_users = User.objects.order_by('?')[:5]
+            # Users start liking some post.
+            random_post = Post.objects.order_by('?').first()
+            for user in random_users:
+                ActivityLog.objects.create(post=random_post, user=user, interaction_type=ActivityLog.LIKE)
+            random_post = Post.objects.order_by('?').first()
+            ActivityLog.objects.create(post=random_post, user=random_users[0], interaction_type=ActivityLog.LIKE)
+            ActivityLog.objects.create(post=random_post, user=random_users[2], interaction_type=ActivityLog.LIKE)
+            ActivityLog.objects.create(post=random_post, user=random_users[4], interaction_type=ActivityLog.LIKE)
+            for _ in range(3):
+                random_post = Post.objects.order_by('?').first()
+                ActivityLog.objects.create(post=random_post, user=random_users[3], interaction_type=ActivityLog.LIKE)
+            # Users start viewing some post.
+            random_post = Post.objects.order_by('?').first()
+            ActivityLog.objects.create(post=random_post, user=random_users[0], interaction_type=ActivityLog.VIEW)
+            ActivityLog.objects.create(post=random_post, user=random_users[2], interaction_type=ActivityLog.VIEW)
+            ActivityLog.objects.create(post=random_post, user=random_users[4], interaction_type=ActivityLog.VIEW)
+            random_post = Post.objects.order_by('?').first()
+            ActivityLog.objects.create(post=random_post, user=random_users[1], interaction_type=ActivityLog.VIEW)
+            ActivityLog.objects.create(post=random_post, user=random_users[3], interaction_type=ActivityLog.VIEW)
+            self.stdout.write(self.style.SUCCESS('Activity Logs created.'))
+        else:
+            self.stdout.write(self.style.WARNING('Activity Logs already created.'))
